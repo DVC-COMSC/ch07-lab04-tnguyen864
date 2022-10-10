1c9b89bdbfc6b70b6394241c9977a76931103388
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 02:38:49 -0700

    Chap7 Lab4

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-
-# ******************************
-# Make your Code
-# ******************************
+# Import random
 import random
 
+# 2 lists, 10 random numbers each
 numbers1 = []
 numbers2 = []
 result = []
 for i in range(10):
 	numbers1.append(random.randint(0,100))
 	numbers2.append(random.randint(0,100))
-print (numbers1)
-print (numbers2)
+
+# List for sum of each list at index
+sumNums = []
+for i in range(10):
+	sumNums.append(numbers1[i] + numbers2[i])
+
+print("First list:",numbers1)
+print("Second list:", numbers2)
+print("Sum of both lists:", sumNums)
